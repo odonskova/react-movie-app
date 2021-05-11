b74b85a9450821f76a98d3d8c390015fc513b34b
Olga Donskova <ohorbal10@gmail.com>
2021-05-11 16:08:48 +0300

    simplify component with icons

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Tooltip} from "reactstrap";
+import { starIcon, bookmarkIcon } from "../../icons/icons";
 
 const Buttons = (props) => {
     const {
@@ -23,33 +24,26 @@ const Buttons = (props) => {
             <button type="button"
                     className="btn btn-outline-warning mr-3"
                     id="rateMovie"
-            ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                     className="bi bi-star-fill mr-1 mb-1" viewBox="0 0 16 16">
-                    <path
-                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
+            >
+                { starIcon }
                 Оценить фильм</button>
             {!user ?
                 <Tooltip placement="bottom" isOpen={rateTooltipOpen} target="rateMovie" toggle={toggleRate}>
-                    Войти для для оценки фильма
+                    Войти для оценки фильма
                 </Tooltip> : null
             }
-
 
             <button type="button"
                     className="btn btn-outline-warning mr-3"
                     id="addToFavorites"
                     onClick={addToFavorites}
-            ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                  className="bi bi-bookmark-star-fill mr-1 mb-1" viewBox="0 0 16 16">
-                <path
-                  d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z"/>
-            </svg>
+            >
+                { bookmarkIcon }
                 Добавить в избранные</button>
 
             {!user ?
                 <Tooltip placement="bottom" isOpen={favoriteTooltipOpen} target="addToFavorites" toggle={toggleFavorite}>
-                    Войти для добавления в свой список избранных
+                    Войти для добавления в список избранных
                 </Tooltip> : null
             }
 
---
+++ src/icons/icons.js
@@ -0,0 +1,33 @@
+export const heartIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red"
+         className="bi bi-heart-fill ml-1 mt-1" viewBox="0 0 20 20">
+        <path fillRule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+    </svg>
+);
+
+export const trashIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+         className="bi bi-trash" viewBox="0 0 20 20">
+        <path
+            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fillRule="evenodd"
+              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+    </svg>
+);
+
+export const starIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+         className="bi bi-star-fill mr-1 mb-1" viewBox="0 0 16 16">
+        <path
+            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+    </svg>
+);
+
+export const bookmarkIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+         className="bi bi-bookmark-star-fill mr-1 mb-1" viewBox="0 0 16 16">
+        <path
+            d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z"/>
+    </svg>
+);
